db1e69833cc33550cde09c45c8d635f14f30b2b7
Nate Berkopec <nate.berkopec@gmail.com>
2024-10-23 14:56:05 +0900

    Add a weird migration with a string column _id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_23_052941) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_23_055531) do
+  create_table "albums", force: :cascade do |t|
+    t.string "title"
+    t.string "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "photos", force: :cascade do |t|
     t.string "title"
     t.integer "album_id"
