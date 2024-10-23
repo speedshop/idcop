5008fd1d711d10cc9708f94e635fc808a35c94b5
Nate Berkopec <nate.berkopec@gmail.com>
2024-10-23 15:09:22 +0900

    Add passing references

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_23_055709) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_23_060908) do
   create_table "albums", force: :cascade do |t|
     t.string "title"
     t.bigint "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
@@ -24,4 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_23_055709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "users"
 end
